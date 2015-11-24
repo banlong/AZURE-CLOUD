f1d30d198300fa91bb64a07da5595ef6319d635a
NGHIEP NGO <nghiepnds@yahoo.com>
2015-11-23 19:26:02 -0800

    Push to branch

--- Contoso/ContosoAdsWeb/WebRole.cs
+++ Contoso/ContosoAdsWeb/WebRole.cs
@@ -11,7 +11,7 @@ namespace ContosoAdsWeb
     {
         public override bool OnStart()
         {
-            // For information on handling configuration changes
+            // For information on handling configuration changes--
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
             return base.OnStart();
